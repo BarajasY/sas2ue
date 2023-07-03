45c5cedafede537f388aeaa1ac57caef99061682
Yahir Moreno Barajas <100240827+BarajasY@users.noreply.github.com>
2023-07-03 13:58:55 -0600

    changed ' with &apos;

--- components/Reason.tsx
+++ components/Reason.tsx
@@ -13,14 +13,14 @@ const Reason = () => {
         <br />
         <p>I believe every single person who is studying or into software development, has ever had this thought of</p>
         <p>dedicating themselves to produce games. Or at least trying out how to build a game, succeeding or failing</p>
-        <p>it really doesn't matter, but I'm convinced everyone has had that thought.</p>
+        <p>it really doesn&apos;t matter, but I&apos;m convinced everyone has had that thought.</p>
         <br />
         <p>Furthermore, before touching Unreal Engine, I had a solid year and a half completely into web development, </p>
-        <p>I can't say i got tired, I just simply wanted to freshen my thoughts and challenge myself to build something</p>
+        <p>I can&apos;t say i got tired, I just simply wanted to freshen my thoughts and challenge myself to build something</p>
         <p>completely different.</p>
         <br />
-        <p>Changing my development tools from Visual Studio Code into Unreal Engine and it's blueprints language. Then</p>
-        <p>trying to figure out how to make models, which honestly I just went with voxels since it's easier for me and</p>
+        <p>Changing my development tools from Visual Studio Code into Unreal Engine and it&apos;s blueprints language. Then</p>
+        <p>trying to figure out how to make models, which honestly I just went with voxels since it&apos;s easier for me and</p>
         <p>the software to make vox models (MagicaVoxel) is pretty light and open source.</p>
         <br />
         <p>Lastly before taking on this callenge of game development, when I saw huge games with many assets and features,</p>
